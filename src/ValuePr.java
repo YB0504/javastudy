0d9a660529d381f664104e75a6367081916047eb
INBEOM YEO <yeo9411@naver.com>
2024-05-03 15:41:33 +0900

    fix: 출력 문자 개행 수정

--- src/ValuePr.java
+++ src/ValuePr.java
@@ -12,7 +12,12 @@ public class ValuePr {
 //        일반 전역 변수는 변수가 선언된 Class의 instance를 생성해야만 접근할 수 있다.
     public void print(){
         // message = 지역변수
-        String message = "전역 변수 name"+name+ "전역 변수 age"+age+"클래스 변수"+breed;
+        String message = "전역 변수 name : "+name+ "\n전역 변수 age : "+age+"\n클래스 변수 : "+breed;
         System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        ValuePr valuePr = new ValuePr();
+        valuePr.print();
     }
 }
