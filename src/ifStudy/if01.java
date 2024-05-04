5b909c3399255139feba381954b183dc7f2f7560
YB0504 <yeo9411@naver.com>
2024-05-04 23:46:12 +0900

    if Remind Start

---
+++ src/ifStudy/if01.java
@@ -0,0 +1,14 @@
+package ifStudy;
+
+public class if01 {
+    public static void main(String[] args) {
+        int score = 80;
+
+        // if Remind 시작
+        // JAVA에서 가장 기본적인 조건문이다.
+        // 조건식의 내용이 true면 아래의 코드를 출력한다.
+        if (score > 60) {
+            System.out.println("합격");
+        }
+    }
+}
