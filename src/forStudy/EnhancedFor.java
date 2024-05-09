449da7f032ef22d4415a5434aac48b339129f5de
INBEOM YEO <yeo9411@naver.com>
2024-05-10 08:50:16 +0900

    Enhanced for 예제

--- src/forStudy/EnhancedFor.java
+++ src/forStudy/EnhancedFor.java
@@ -6,5 +6,10 @@ public class EnhancedFor {
 //        for (타입 변수 : 배열, 컬렉션) {
 //            실행문;
 //        }
+        String[] Array = {"1-1", "1-2", "1-3", "1-4", "1-5"};
+
+        for (String str : Array) {
+            System.out.println(str);
+        }
     }
 }
