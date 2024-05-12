891841a07d20453aa4327f21dfe85b9270ebf7a4
YB0504 <yeo9411@naver.com>
2024-05-12 22:34:35 +0900

    일반 for문 향상된 for문으로 변경

--- src/ArrayStudy/Array02.java
+++ src/ArrayStudy/Array02.java
@@ -4,15 +4,21 @@ public class Array02 {
     public static void main(String[] args) {
         // 총합, 평균
         int sum = 0;
-        float avg = 0f;
+        double avg = 0f;
 
         int[] score = {100, 88, 100, 100, 90};
 
-        for (int i = 0; i < score.length; i++) {
-            sum += score[i];
+//        for (int i = 0; i < score.length; i++) {
+//            sum += score[i];
+//        }
+
+        // 향상된 for문으로 바꾸기
+        for (int scores : score) {
+            sum += scores;
         }
+
         // 총합을 배열의 길이로 나눈다.
-        avg = sum / (float) score.length;
+        avg = sum / (double) score.length;
 
         System.out.println("총합 : " + sum);
         System.out.println("평균 : " + avg);
