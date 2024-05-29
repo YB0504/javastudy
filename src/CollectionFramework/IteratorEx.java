1d921437586a83a1fdc8967cc1f3568e8c61a3d7
INBEOM YEO <yeo9411@naver.com>
2024-05-29 13:37:44 +0900

    ArraysMethod

---
+++ src/CollectionFramework/IteratorEx.java
@@ -0,0 +1,22 @@
+package CollectionFramework;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IteratorEx {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+
+        Iterator iterator = list.iterator();
+
+        while (iterator.hasNext()) {
+            Object o = iterator.next();
+            System.out.println(o);
+        }
+    }
+}
